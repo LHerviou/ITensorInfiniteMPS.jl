70ba259b2d087551faa9b110351448b5fda546c4
LHerviou <loic.herviou@epfl.ch>
2022-02-09 14:01:35 +0100

    Changing towards translatecells

--- src/vumps_localham.jl
+++ src/vumps_localham.jl
@@ -217,7 +217,7 @@ function (A::Aᴸ)(x)
   r = linkinds(only, ψ.AR)
   r′ = linkinds(only, ψ′.AR)
 
-  xT = translatecell(x, -1)
+  xT = translatecell(translater(ψ), x, -1)
   for k in (n - N + 1):n
     xT = xT * ψ.AL[k] * ψ̃.AL[k]
   end
@@ -330,7 +330,7 @@ function (A::Aᴿ)(x)
   for k in reverse(1:N)
     xT = xT * ψ.AR[k] * ψ̃.AR[k]
   end
-  xT = translatecell(xT, 1)
+  xT = translatecell(translater(ψ), xT, 1)
   δˡ = δ(l[n], l′[n])
   δʳ = δ(r[n], r′[n])
   xR = x * ψ.C[n] * ψ′.C[n] * δˡ * denseblocks(dag(δʳ))
