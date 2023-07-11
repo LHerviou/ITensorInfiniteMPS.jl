32f15643ebb84645dd18c68afba66bc98452d85c
LHerviou <loic.herviou@epfl.ch>
2023-07-11 12:11:18 +0200

    Removing debugging

--- src/idmrg_auxiliary.jl
+++ src/idmrg_auxiliary.jl
@@ -32,15 +32,13 @@ function LinearAlgebra.qr(A::ITensor, Linds...; kwargs...)
   settags!(Q, tags, q)
   settags!(R, tags, q)
   q = settags(q, tags)
-  println("Iteration")
-  println("Tensor Q of norm $(norm(Q))")
-  println(inds(Q));
-  println("Tensor R of norm $(norm(R))")
-  println(inds(R));
-  println("Center Index")
-  println(q);
-  #println(left_combiner);
-  #println(right_combiner);
+  # println("Iteration")
+  # println("Tensor Q of norm $(norm(Q))")
+  # println(inds(Q));
+  # println("Tensor R of norm $(norm(R))")
+  # println(inds(R));
+  # println("Center Index")
+  # println(q);
   return dag(left_combiner) * Q, R * dag(right_combiner), q
 end
 
