fe4fb29a9bc1ca64010b5cf25d1d975f251b2ef1
LHerviou <loic.herviou@epfl.ch>
2023-09-29 17:58:03 +0200

    Adding PH sym

--- src/models/fqhe_gen.jl
+++ src/models/fqhe_gen.jl
@@ -2,10 +2,10 @@
 # loic.herviou@epfl.ch
 ###############################
 function unit_cell_terms(
-  ::Model"fqhe_gen"; dict_coeffs = Dict{NTuple{4, Int64}, Number}(), prec = 1e-8, L = -1
+  ::Model"fqhe_gen"; dict_coeffs = Dict{NTuple{4, Int64}, Number}(), prec = 1e-8, L = -1, PHsym = false
 )
   #we assume that dict_coeffs has already been filtered, and is ready to be used
-  opt = optimize_coefficients(dict_coeffs; prec=prec)
+  opt = optimize_coefficients(dict_coeffs; prec, PHsym)
   ops = generate_Hamiltonian(opt)
   if L != -1
     for j in 1:L
